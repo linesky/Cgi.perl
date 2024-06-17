e20feab1a03ebcf55a243aa6e2eb5f4d01989de5
sky line <32512284+linesky@users.noreply.github.com>
2024-06-17 22:42:45 +0100

    Add files via upload

---
+++ nl.py
@@ -0,0 +1,20 @@
+import sys
+
+
+
+def readstdin(arg1:int):
+    counters=0
+    for s in sys.stdin:
+        counters+=1
+        print(str(counters)+"\t"+s)
+    
+
+    
+print("\x1b[43;37m")
+if 0==0:
+    readstdin(0)
+
+
+
+
+
