01f7dc1c0a229d0739356585c8b9453594bb0393
sky line <32512284+linesky@users.noreply.github.com>
2024-06-17 21:03:45 +0100

    Add files via upload

---
+++ fors.py
@@ -0,0 +1,17 @@
+import subprocess
+import sys
+def readstdin(arg1:int,arg2:int,arg3:str,arg4:str):
+    
+    for n in range(arg1,arg2):
+        subprocess.run([arg3 , arg4,str(n)])
+    
+
+    
+print("\x1b[43;37m")
+if len(sys.argv)>3:
+    readstdin(int(sys.argv[1]),int(sys.argv[2]),sys.argv[3],sys.argv[3])
+
+
+
+
+
