570dad8b15e180ce9ad1f27f50d865f8c58c5238
sky line <32512284+linesky@users.noreply.github.com>
2024-06-17 23:22:57 +0100

    Add files via upload

---
+++ shorts.py
@@ -0,0 +1,26 @@
+import sys
+        
+
+
+def readstdin(arg1:int):
+    ss=[]
+    for s1 in sys.stdin:
+        ss=ss+[s1]
+    ss.sort()
+    
+    
+    ss.sort()
+    for s1 in ss:
+        
+        print(s1)
+    
+
+    
+print("\x1b[43;37m")
+if 0==0:
+    readstdin(0)
+
+
+
+
+
